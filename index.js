5eeeea6a27e65f2032b06163c8337120aee935a5
skramchez <skramchez100500@gmail.com>
2022-04-25 22:51:23 +0300

    fix routing, add step function

--- index.js
+++ index.js
@@ -9,34 +9,19 @@ import ChessGame from './models/ChessGame.js'
 import authRouter from './routers/authRouter.js'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const httpServer = createServer();
-const io = new Server(httpServer,{ cors: { origin: '*' }});
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer,{ cors: { origin: '*' }});
 
 app.use(express.json()) 
-app.use(cors) 
+app.use(cors()) 
 
 const DB_URL = 'mongodb+srv://ChessRoot:7Ms7nOs1UGliAC4a@cluster0.rhguy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-async function startApp() {
-    try {
-        await mongoose.connect(DB_URL, { useUnifiedTopology: true, UseNewUrlParser: true })
-        httpServer.listen(3333, () => {
-            console.log('Application listening on port 3333!')
-        })
-    } catch (e) {
-        console.log(e)
-    }
-}
-
-
 const Game = new ChessGame()  // Головний об'єкт гри
 
-io.on('connection', socket =>{
-    console.log('User connected')   
-
-    socket.on('start', (data) => { 
-        console.log('start event')        
+io.on('connection', socket =>{ console.log('User connected'); 
+    socket.on('start', (data) => {                          // { user, white }      
         if (Game.users[socket.id] !== undefined) return;    // Перевірка що гравець ще не в грі
         Game.start({ id: socket.id.toString(), ...data }, (start, gameId, opponent, gameBoard) => {     
             if (start) {                                    // Перевірка початку гри
@@ -49,6 +34,13 @@ io.on('connection', socket =>{
             }
         }) 
     })
+    socket.on('step', (data) => {                           // { gameId,  from,  to }
+       const gameStep = Game.games[data.gameId].step(data.from, data.to)
+        if  (gameStep != null) {
+            io.to(Game.games[data.gameId].getUserId()).emit('upDateBoard', {gameBoard})
+            io.to(Game.games[data.gameId].getOpponentrId()).emit('upDateBoard', {gameBoard})
+        }
+    })
 }) 
 
 app.use('/api/auth', authRouter);
@@ -58,5 +50,14 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 });
 
-
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL, { useUnifiedTopology: true, UseNewUrlParser: true })
+        httpServer.listen(3333, () => {
+            console.log('Application listening on port 3333!')
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
 startApp()
