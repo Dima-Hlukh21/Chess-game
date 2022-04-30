b574b11c5c226bc1ad7f292c6610b4d8f6ec7134
skramchez <skramchez100500@gmail.com>
2022-04-30 11:44:01 +0300

    optimization

--- models/GameItem.js
+++ models/GameItem.js
@@ -10,6 +10,30 @@ export default  class GameItem{
     }
     getBoard(){
         return this.gameModule.board()
+    } 
+    visibilityBoard(){
+        const possibleMovesW = [];
+        const possibleMovesB = [];
+        this.getBoard().map((item,index) => {
+            item.map((cell,index) => {
+                if(cell == null) return;
+                if (cell.color = "w"){
+                    possibleMovesW.push(...this.gameModule.moves({scuare: cell.square}) )
+                } else{
+                    possibleMovesB.push(...this.gameModule.moves({scuare: cell.square}) ) 
+                };
+            });
+        } )
+        const possibleMovesWfiltred = possibleMovesW.filter((element, index) => {
+            return possibleMovesW.indexOf(element) === index;
+        });
+        const possibleMovesBfiltred = possibleMovesB.filter((element, index) => {
+            return possibleMovesB.indexOf(element) === index;
+        });
+        return possibleMovesWfiltred;
+    }
+    boardToConsole(){
+        return this.gameModule.ascii()
     }
     step(from, to){
         return this.gameModule.move({from,to})
