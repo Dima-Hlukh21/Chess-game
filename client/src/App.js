25aa5b595f2f11cc6486ff88a9afb848b0e2b0fb
Dima-Hlukh21 <dimon18112000@gmail.com>
2022-05-02 09:31:35 +0300

    pieces

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,33 @@ import {SocketContext, socket} from './components/Socket';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import CreateBord from './components/Board';
+import { GameContext } from './components/GameContext';
+
+
 
 function App() {
+  const [gameState, setGameState] = React.useState({gameboard: []});
+
+  function updateGameboard(newBoard){
+    setGameState({...gameState, gameboard: newBoard})
+  }
+  
+  const gameValue = {
+    gameboard: gameState.gameboard,
+    updateGameboard: updateGameboard
+  };
+
+
   return (
     <SocketContext.Provider value={socket}>
- <BrowserRouter basename="/">
- <Routes>
-
-     
-      <Route path="/" element={ <FirstPage/> } />
-      <Route path="/game" element={ <CreateBord/> } />
-      </Routes>
-
-      </BrowserRouter>
-
+      <GameContext.Provider value={gameValue}>
+        <BrowserRouter basename="/">
+        <Routes>     
+          <Route path="/" element={ <FirstPage/> } />
+          <Route path="/game" element={ <CreateBord/> } />
+        </Routes>
+        </BrowserRouter>
+      </GameContext.Provider>
     </SocketContext.Provider>
 
   );
--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,48 +1,41 @@
 import React from 'react';
+import { GameContext } from './GameContext';
 
 
+function Cell({boardCellData, cellIndex, rowIndex, children}) {
 
-
-
-
-
-
-
-function Cell({width, height, position, children}) {
-  const offsetX = position.x * width;
-  const offsetY = position.y * height;
-
-  const isBlack = position.x % 2 === position.y % 2;
-
+  let square, color, type;
+type.map((type, color)=>{
+  
+})
+  if(boardCellData) {square = boardCellData.square; color = boardCellData.color; type = boardCellData.type;}
+  // type.map((type, color) => {
+  //   if(type, color) {}
+  
+  // })
+  type= <img src="../img/Pawn B.png"></img>
   return <div style={{
-    position: 'absolute',
-    width: width + 'px',
-    height: height + 'px',
-    transform: `translate(${offsetX}px, ${offsetY}px)`,
-    backgroundColor: isBlack? 'lightgray': 'lightyellow'
-  }}>{children}</div>
+    display: "inline-block",
+    width: '75px',
+    height: '75px',
+    backgroundColor: ((cellIndex  +rowIndex) % 2)? 'lightgray': 'lightyellow'
+  }}>{type} {color}</div>
+}
+
+function Row({boardRowData, rowIndex, children}) {
+  return <div>  
+    {boardRowData.map((value, index) => {
+      return <Cell boardCellData={value} cellIndex={index} rowIndex={rowIndex} />
+    })}  
+  </div>
 }
 
 function CreateBord() {
+
+  const { gameboard } = React.useContext(GameContext);
+
   const width = 600;
   const height = 600;
-
-  const numberOfRows = 8;
-  const numberOfColumns = 8;
-
-  const cellWidth = width / numberOfColumns;
-  const cellHeight = height / numberOfRows;
-
-  const board = [
-    2,3,4,5,6,4,3,2,
-    1,1,1,1,1,1,1,1,
-    0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,
-    -1,-1,-1,-1,-1,-1,-1,-1,
-    -2,-3,-4,-5,-6,-4,-3,-2
-  ];
   
   return (
     <div
@@ -51,18 +44,11 @@ function CreateBord() {
         width: width + 'px',
         height: height + 'px',
       }}>
-      {board.map((v,i) => {
-        const position = {
-          x: i % numberOfColumns,
-          y: (i - (i % numberOfColumns)) / numberOfColumns,
-        };
 
-        return <Cell 
-          width={cellWidth}
-          height={cellHeight}
-          position={position}
-          >{}</Cell>
+      {gameboard.map((value, index) => {
+          return <Row boardRowData={value} rowIndex={index}/>
       })}
+
     </div>
   );
 }
